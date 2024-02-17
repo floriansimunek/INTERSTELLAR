340a1e3c0786d85ed0abc61ead231eefa17265ba
Florian SIMUNEK <florian.simunek@gmail.com>
2024-02-17 01:43:46 +0100

    🐛 Fix parallax

--- parallax.js
+++ parallax.js
@@ -6,16 +6,23 @@ window.onload = () => {
   const container = document.querySelector('.background__container');
   const image = document.querySelector('.background__image');
 
-  container.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-    updatePosition();
-  });
+  setTimeout(() => {
+    container.addEventListener('mousemove', (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+      updatePosition();
+    });
 
-  function updatePosition() {
-    const targetOffsetX = (container.offsetWidth / 2 - mouseX) / DIVIDER;
-    const targetOffsetY = (container.offsetHeight / 2 - mouseY) / DIVIDER;
+    function updatePosition() {
+      const targetOffsetX = (container.offsetWidth / 2 - mouseX) / DIVIDER;
+      const targetOffsetY = (container.offsetHeight / 2 - mouseY) / DIVIDER;
 
-    image.style.transform = `translate(${targetOffsetX}px, ${targetOffsetY}px)`;
-  }
+      image.style.setProperty(
+        'transform',
+        `translate(${targetOffsetX}px, ${targetOffsetY}px)`,
+        'important',
+      );
+      image.style.scale = 1.2;
+    }
+  }, 1250);
 };
